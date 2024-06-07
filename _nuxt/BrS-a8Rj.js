529673440d0b7c9936e5b30ac9899dc0ae64d9cc
Ed Khalturin <ekhalturin@oggettoweb.com>
2024-06-07 16:05:58 +0300

    dvegasa-ru-nuxt release!

---
+++ _nuxt/BrS-a8Rj.js
@@ -0,0 +1,1 @@
+import{n as a,q as s,o as n,c as o,s as r}from"./BanVczU6.js";import{E as _}from"./CqG4hWE2.js";const c={class:"PageXp"},f=a({__name:"PageXp",async setup(p){let e,t;return[e,t]=s(()=>r(_.xp,{external:!0})),await e,t(),(i,l)=>(n(),o("div",c))}});export{f as default};
---
+++ _nuxt/D8BBnZGg.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DHKDqXja.js";import"./BanVczU6.js";export{m as default};
