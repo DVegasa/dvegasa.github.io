9c9751f0bdc2856ff254c7000fbb2b50fa4f13a8
Ed Khalturin <dracveg@gmail.com>
2024-06-17 20:46:06 +0300

    deploy

---
+++ _nuxt/Bd-sXmgk.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Yu_N4-xO.js";import"./BKu-_mEQ.js";export{m as default};
---
+++ _nuxt/CqG4hWE2.js
@@ -0,0 +1,1 @@
+const t={cv:"https://drive.google.com/file/d/1HKAtFmZcGReYVL7Q8sNidvGesTF1IZNS/view",xp:"https://docs.google.com/document/d/15PEcB9D5pG8sAzj43YspY0YUkq9fwFi1xno7ZY3ZZUM",telegram:"https://t.me/dvegasa",linkedin:"https://www.linkedin.com/in/dvegasa/",github:"https://github.com/dvegasa/"};export{t as E};
